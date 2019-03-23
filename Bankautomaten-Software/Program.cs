df66cd2ea66e7891596594adf89ccc2c51c55e87
JanHen34 <jannibremen@gmail.com>
2019-03-23 22:00:18 +0100

    Refactoring main method

--- Bankautomaten-Software/Program.cs
+++ Bankautomaten-Software/Program.cs
@@ -8,88 +8,112 @@ namespace Bankautomaten_Software
 {
     class Program
     {
+        static int pin;
+        static bool pin_anforderung;
+        static string eingabe;
+        static string eingabe_2;
+        static string währung = "Euro";
+        static double kontostand = 1000;
+        static string bearbeitung_fortsetzen;
+        static string name_kunde;
+
+        static double betrag_einzahlen;
+        static double betrag_auszahlen;
+
+
         static void Main(string[] args)
         {
-            int pin;
-            bool pin_anforderung;
-            string eingabe;
-            string eingabe_2;
-            string währung = "Euro";
-            double kontostand = 1000;
-            string bearbeitung_fortsetzen;
-            string name_kunde;
-
-            int betrag_einzahlen;
-            int betrag_auszahlen;
-
             Console.WriteLine("Willkommen bei der Sparkasse!");
-            Console.WriteLine("Bitte geben sie Ihre 4 stelligen PIN ein um fortzufahren!");            
+            Console.WriteLine("Bitte geben sie Ihre 4 stelligen PIN ein um fortzufahren!");
             pin = Convert.ToInt32(Console.ReadLine());
-            while (pin > 9999 == true)
+
+            //1,2,3,4..... >||1111,11112,1113..... 9998,9999||< 10000
+            while (pin < 1111 || pin > 9999 )
             {
-                Console.WriteLine("Die angegebene PIN einspricht nicht den Anforderungen! Bitte geben sie ihre PIN erneut ein!");
-                pin = Convert.ToInt32(Console.ReadLine());              
+                pin = pinUeberpruefung();
             }
-            while (pin < 1111 == true)
-            {
-                Console.WriteLine("Die angegebene PIN entspricht nicht den Anforderungen! Bitte geben sie Ihre PIN erneut ein!");
-                pin = Convert.ToInt32(Console.ReadLine());                              
-            }
-            Console.WriteLine("Bitte wählen Sie aus den 4 Möglichkeiten: Einzahlen, Auszahlen und Kontostand aus!");
+
+            ichSchreibeEineNeueMethode(kontostand);
+
+            Console.WriteLine("Bitte wähen Sie aus den 3 Möglichkeiten: Einzahlen, Auszahlen und Kontostand aus!");
             eingabe = Console.ReadLine();
-            switch (eingabe)
-            {
-                case "Einzahlen" :
-                    Console.WriteLine("Bitte geben sie den gewünschten Betrag ein, den sie einzahlen möchten.");
-                    betrag_einzahlen = Convert.ToInt32(Console.ReadLine());
-                    kontostand = kontostand + betrag_einzahlen;
-                    Console.WriteLine("Ihr Kontostand wurde Aktualisiert. Bitte Sie Ihr einzuzahlendes Geld in die Vorrichtung!");
-                break;
-                case "Auszahlen":
-                    Console.WriteLine("Bitte geben sie den gewünschten Betrag ein, den sie auszahlen möchten.");
-                    betrag_auszahlen = Convert.ToInt32(Console.ReadLine());
-                    kontostand = kontostand - betrag_auszahlen;
-                    Console.WriteLine("Ihr Kontostand wurde aktualisiert. Bitte entnehmen Sie ihr Geld!");
-                break;
-                case "Kontostand":
-                    Console.WriteLine("Ihr Kontostand beträgt " + kontostand + " " + währung + ".");
-                break;
-            }
+            starteBankProzess(eingabe, währung, kontostand);
             Console.WriteLine("Wollen Sie die Bearbeitung fortsetzen?");
             bearbeitung_fortsetzen = Console.ReadLine();
             while (bearbeitung_fortsetzen == "Ja")
             {
                 Console.WriteLine("Bitte wählen Sie aus den 3 Möglichkeiten: Einzahlen, Auszahlen und Kontostand aus!");
                 eingabe = Console.ReadLine();
-                switch (eingabe)
-                {
-                    case "Kontostand":
-                        Console.WriteLine("Ihr Kontostand beträgt " + kontostand + " " + währung + ".");
-                    break;
-                    case "Einzahlen":
-                        Console.WriteLine("Bitte geben sie den gewünschten Betrag ein, den sie einzahlen möchten.");
-                        betrag_einzahlen = Convert.ToInt32(Console.ReadLine());
-                        kontostand = kontostand + betrag_einzahlen;
-                        Console.WriteLine("Ihr Kontostand wurde Aktualisiert. Bitte Sie Ihr einzuzahlendes Geld in die Vorrichtung!");
-                    break;
-                    case "Auszahlen":
-                        Console.WriteLine("Bitte geben sie den gewünschten Betrag ein, den sie auszahlen möchten.");
-                        betrag_auszahlen = Convert.ToInt32(Console.ReadLine());
-                        kontostand = kontostand - betrag_auszahlen;
-                        Console.WriteLine("Ihr Kontostand wurde aktualisiert. Bitte entnehmen Sie ihr Geld!");
-                    break;
-                }
+                starteBankProzess(eingabe, währung, kontostand);
                 Console.WriteLine("Wollen sie die Bearbeitung fortsetzen?");
                 bearbeitung_fortsetzen = Console.ReadLine();
                 if (bearbeitung_fortsetzen == "Nein")
                 {
-                    break;                    
+                    break;
                 }
-            }           
+            }
             Console.WriteLine("Wir wünschen Ihnen noch einen schönen Tag! Bitte drücken Sie eine Taste!");
             Console.ReadKey();
+        }
 
-            
+        private static void ichSchreibeEineNeueMethode(double kontostand)
+        {
+            Console.WriteLine("yolo");
+        }
+
+        private static int pinUeberpruefung()
+        {
+            int pin;
+            Console.WriteLine("Die angegebene PIN einspricht nicht den Anforderungen! Bitte geben sie ihre PIN erneut ein!");
+            pin = Convert.ToInt32(Console.ReadLine());
+            return pin;
+        }
+
+        private static void starteBankProzess(string eingabe, string währung, double kontostand)
+        {
+            switch (eingabe)
+            {
+                case "Einzahlen":
+                    starteEinzahlen(kontostand);
+                    break;
+                case "Auszahlen":
+                    starteAuszahlen(kontostand);
+                    break;
+                case "Kontostand":
+                    Console.WriteLine("Ihr Kontostand beträgt " + kontostand + " " + währung + ".");
+                    break;
+            }
+        }
+
+        private static double starteAuszahlen(double kontostand)
+        {
+            double betrag_auszahlen;
+            Console.WriteLine("Bitte geben sie den gewünschten Betrag ein, den sie auszahlen möchten.");
+            betrag_auszahlen = Convert.ToDouble(Console.ReadLine());
+            kontostand = fuehreAuzahleAus(kontostand, betrag_auszahlen);
+            Console.WriteLine("Ihr Kontostand wurde aktualisiert. Bitte entnehmen Sie ihr Geld!");
+            return betrag_auszahlen;
+        }
+
+        private static double starteEinzahlen(double kontostand)
+        {
+            double betrag_einzahlen;
+            Console.WriteLine("Bitte geben sie den gewünschten Betrag ein, den sie einzahlen möchten.");
+            betrag_einzahlen = Convert.ToDouble(Console.ReadLine());
+            kontostand = fuehreEinzahlenAus(kontostand, betrag_einzahlen);
+            Console.WriteLine("Ihr Kontostand wurde Aktualisiert. Bitte Sie Ihr einzuzahlendes Geld in die Vorrichtung!");
+            return betrag_einzahlen;
+        }
+
+        static double fuehreEinzahlenAus(double kontostand, double betrag_einzahlen)
+        {
+            return kontostand = kontostand + betrag_einzahlen;
+        }
+
+        static double fuehreAuzahleAus(double betrag_auszahlen, double kontostand)
+        {               
+            return kontostand = kontostand - betrag_auszahlen;          
         }
     }
 }
+
